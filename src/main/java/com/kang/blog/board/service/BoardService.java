5ef17d9e0551388b4f30ae177a0b670e181d89b7
kang4609 <kang4609@gmail.com>
2020-03-25 20:01:18 +0900

    commit

--- src/main/java/com/kang/blog/board/service/BoardService.java
+++ src/main/java/com/kang/blog/board/service/BoardService.java
@@ -1,20 +1,22 @@
 package com.kang.blog.board.service;
 
-import com.kang.blog.board.repository.BoardRepository;
-import com.kang.blog.board.entity.BoardEntity;
 import com.kang.blog.board.dto.BoardDto;
+import com.kang.blog.board.repository.BoardRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
     public Long savePost(BoardDto boardDto) {
+        log.debug("gid = > " );
         return boardRepository.save(boardDto.toEntity()).getId();
     }
 }
